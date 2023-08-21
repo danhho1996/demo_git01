84c3b36407122e2558ff6eff0aca5517c3d12d21
danhho1996 <danhhoitk15@gmail.com>
2023-08-21 10:00:38 +0700

    danh commit nè

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'https://docs.google.com/document/d/1MBCvHjOBP3veCPWUm8uAdHiJZ982smxa8fcvqc-lxqs/edit';
   }
 }
